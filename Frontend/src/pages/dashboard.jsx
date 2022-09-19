e7e0207f8e6edbaf89c4a3fe77c8b5dfaa646316
Nadav <nadavvivsa@gmail.com.com>
2022-09-20 01:23:16 +0300

    a

--- Frontend/src/pages/dashboard.jsx
+++ Frontend/src/pages/dashboard.jsx
@@ -9,91 +9,90 @@ import { updateUser } from "../store/user/user.action"
 export const Dashboard = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const {loggedInUser} = useSelector(state=> state.userModule)
-    const [deleteModal,setDeleteModal] = useState(false)
-    const [site,setSite] = useState(loggedInUser.waps[0])
-    const siteRef = useRef()
+    const { loggedInUser } = useSelector(state => state.userModule)
 
-    useEffect(()=>{
-        siteRef.current = site
-    },[site])
-    if(!loggedInUser) {
-        navigate('/')
-        return
-    }   
+    if (!loggedInUser) navigate('/')
+    
+    const [deleteModal, setDeleteModal] = useState(false)
+    const [site, setSite] = useState(loggedInUser?.waps[0])
 
-   
 
-    const onEdit = ()=>{
+
+
+
+
+
+
+    const onEdit = () => {
         site.editCount++
         dispatch(setDraft(site))
         navigate(`/editor/${site._id}`)
     }
 
-    const onDelete=(ev)=>{
+    const onDelete = (ev) => {
         ev.preventDefault()
         const name = ev.target[0].value
-        if(name === site.siteName){
+        if (name === site.siteName) {
             setDeleteModal(false)
             const user = loggedInUser
-            const wapIndex  = user.waps.findIndex(wap=> wap._id === site._id)
-            user.waps.splice(wapIndex,1)
+            const wapIndex = user.waps.findIndex(wap => wap._id === site._id)
+            user.waps.splice(wapIndex, 1)
             dispatch(updateUser(user))
             showSuccessMsg('Website has been deleted!')
 
         }
 
     }
-
-    if(!site) return <section className="no-sites"><h1 >No sites to show...</h1> <button onClick={()=>{navigate('/templates')}}>Let's build some!</button></section>
+    if(!loggedInUser) return
+    if (!site) return <section className="no-sites"><h1 >No sites to show...</h1> <button onClick={() => { navigate('/templates') }}>Let's build some!</button></section>
     return (
         <section className="dashboard-page">
             {deleteModal && <div className="delete-modal-container">
-                <div onClick={()=>setDeleteModal(false)} className="black-screen"></div>
+                <div onClick={() => setDeleteModal(false)} className="black-screen"></div>
                 <div className="delete-modal">
                     <h1>To delete the website, Write "{site.siteName}" in the text field and submit:</h1>
                     <form onSubmit={onDelete}><input maxLength={14} type="text" placeholder="Website name here..." /> <button>Delete</button></form>
                 </div>
             </div>}
-        <div className="sites-nav">
-            <h1 className="list-title">My Sites</h1>
-            <ul className="site-list">
-            {loggedInUser.waps.map(wap=> {
-                return <div className="list-item"> <img src={require('../assets/img/icons/website-icon.svg').default} alt="website-icon" /> <li onClick={()=>setSite(wap)} key={wap._id} className={wap._id}>{wap.siteName}</li></div>
-            })}
-            </ul>
+            <div className="sites-nav">
+                <h1 className="list-title">My Sites</h1>
+                <ul className="site-list">
+                    {loggedInUser.waps.map(wap => {
+                        return <div key={wap._id} className="list-item"> <img src={require('../assets/img/icons/website-icon.svg').default} alt="website-icon" /> <li onClick={() => setSite(wap)} key={wap._id} className={wap._id}>{wap.siteName}</li></div>
+                    })}
+                </ul>
 
-        </div>
+            </div>
             <div className="action-btns">
-        <button className="visit-btn">Visit</button>
-        <button onClick={()=>{onEdit()}} className="edit-btn">Edit</button>
-        <button onClick={()=>{setDeleteModal(true)}} className="delete-btn">Delete website</button>
-        </div>
-        <div className="details-grid">
-        <div className="site-name">
-            <h1>{site.siteName}</h1>
-            <h1>Website name</h1>
-            <img src={require('../assets/img/icons/dash-website-icon.svg').default} alt="website-icon" />
-        </div>
-        <div className="site-edits">
-      
-            <h1>      {site.editCount}</h1>
-            <h1>Edit count</h1>
-            <img src={require('../assets/img/icons/dash-edit-icon.svg').default} alt="edit-icon" />
-        </div>
-        <div className="site-views">
-            <h1>{site.viewCount}</h1>
-            <h1>Views count</h1>
-            <img src={require('../assets/img/icons/dash-views-icon.svg').default} alt="views-icon" />
-            
-        </div>
-        <div className="site-creation-date">
-            <h1>{site.createdAt}</h1>
-            <h1>Website creation date</h1>
-            <img src={require('../assets/img/icons/dash-calendar-icon.svg').default} alt="calendar-icon" />
-            
-        </div>
-        </div>
+                <button className="visit-btn">Visit</button>
+                <button onClick={() => { onEdit() }} className="edit-btn">Edit</button>
+                <button onClick={() => { setDeleteModal(true) }} className="delete-btn">Delete website</button>
+            </div>
+            <div className="details-grid">
+                <div className="site-name">
+                    <h1>{site.siteName}</h1>
+                    <h1>Website name</h1>
+                    <img src={require('../assets/img/icons/dash-website-icon.svg').default} alt="website-icon" />
+                </div>
+                <div className="site-edits">
+
+                    <h1>      {site.editCount}</h1>
+                    <h1>Edit count</h1>
+                    <img src={require('../assets/img/icons/dash-edit-icon.svg').default} alt="edit-icon" />
+                </div>
+                <div className="site-views">
+                    <h1>{site.viewCount}</h1>
+                    <h1>Views count</h1>
+                    <img src={require('../assets/img/icons/dash-views-icon.svg').default} alt="views-icon" />
+
+                </div>
+                <div className="site-creation-date">
+                    <h1>{site.createdAt}</h1>
+                    <h1>Website creation date</h1>
+                    <img src={require('../assets/img/icons/dash-calendar-icon.svg').default} alt="calendar-icon" />
+
+                </div>
+            </div>
         </section>
     )
 }
