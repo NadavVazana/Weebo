91b239d826cf5931db705715978d2c7538d7f2a7
Nadav <nadavvivsa@gmail.com.com>
2022-09-19 10:00:33 +0300

    a

--- Frontend/src/cmps/container.jsx
+++ Frontend/src/cmps/container.jsx
@@ -1,14 +1,14 @@
 import { useSelector } from "react-redux"
 import { DynamicCmp } from "./dynamic-cmp"
-
+import { useState } from "react"
 
 export function Container({ cmp, onEditElement }) {
     const { cmps, name } = cmp
     const currElement = useSelector(state => state.draftModule.currElement)
-
+    const [hover,setHover] = useState(false)
     return (
-        <section onClick={() => onEditElement(cmp)} >
-            <div className={`container ${name} ${cmp.id === currElement?.id ? 'focus' : ''}`}>
+        <section onMouseLeave={()=>setHover(false)} onMouseEnter={()=>setHover(true)} onClick={() => onEditElement(cmp)} >
+            <div className={`container ${name} ${cmp.id === currElement?.id ? 'focus' : ''} ${hover && 'cmp-hovered'}`}>
                 {cmps.map((cmp) => <DynamicCmp key={cmp.id} cmp={cmp} onEditElement={onEditElement} />)}
             </div>
         </section>
