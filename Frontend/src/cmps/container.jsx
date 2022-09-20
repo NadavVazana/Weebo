8395e991616a786210f416617c059c1d084dd1f3
Nadav <nadavvivsa@gmail.com.com>
2022-09-20 13:15:25 +0300

    publish start

--- Frontend/src/cmps/container.jsx
+++ Frontend/src/cmps/container.jsx
@@ -1,19 +1,24 @@
 import { useSelector } from "react-redux"
 import { DynamicCmp } from "./dynamic-cmp"
 import { useState } from "react"
-
-export function Container({ cmp, onEditElement }) {
+export function Container({ cmp, onEditElement,isPublished }) {
     const { cmps, name } = cmp
     const currElement = useSelector(state => state.draftModule.currElement)
     const [hover,setHover] = useState(false)
     return (
-        <section onMouseLeave={()=>setHover(false)} onMouseEnter={()=>setHover(true)} onClick={() => onEditElement(cmp)} >
+        <section>
+            {!isPublished ? <section onMouseLeave={()=>setHover(false)} onMouseEnter={()=>setHover(true)} onClick={() => onEditElement(cmp)} >
             <div 
             className={`container ${name} ${cmp.id === currElement?.id ? 'focus' : ''} ${hover && 'cmp-hovered'}`}
             style={{ ...cmp?.styles }}
             >
-                {cmps.map((cmp) => <DynamicCmp key={cmp.id} cmp={cmp} onEditElement={onEditElement} />)}
+                {cmps.map((cmp) => <DynamicCmp isPublished={isPublished} key={cmp.id} cmp={cmp} onEditElement={onEditElement} />)}
             </div>
+        </section> : <section></section> }
+
         </section>
+
+
+
     )
 }
