7ae55646caf10ff598fcdda59948e6c599f19b5e
Nadav <nadavvivsa@gmail.com.com>
2022-09-21 11:31:11 +0300

    a

--- Frontend/src/cmps/views-chart.jsx
+++ Frontend/src/cmps/views-chart.jsx
@@ -5,11 +5,11 @@ import { Pie } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 export const data = {
-  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  labels: ['January', 'February ', 'March', 'April', 'May', 'June','July','August','September','October','November','December'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      data: [12, 19, 3, 5, 2, 3,6,3,8,2,3,4],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
